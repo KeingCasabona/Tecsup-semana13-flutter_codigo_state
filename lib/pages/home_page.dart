71882afa5ecda1231fda7112fced53bf6673eae6
KeingCasabona <keing.limache.casabona@gmail.com>
2024-11-20 19:37:11 -0500

    feat: Structuring our project 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,17 +7,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => RegisterPage()));
-          },
-          child: Icon(
-            Icons.add,
-          )),
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => RegisterPage()));
+            },
+            child: Icon(
+              Icons.add,
+            )),
+        appBar: AppBar(
+          title: const Text('Home Page'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Nombre:',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Apellido:',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Dirección:',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ));
   }
 }
